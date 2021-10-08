45794f8b25e7bdf87c221def44d27b3e99a0eb3f
Adrian Liechti <adrian@monostream.com>
2021-10-08 18:47:42 +0200

    move some code

--- app/remote/remote_shell.go
+++ app/remote/remote_shell.go
@@ -2,12 +2,18 @@ package remote
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/adrianliechti/loop/app"
 	"github.com/adrianliechti/loop/pkg/cli"
+	"github.com/adrianliechti/loop/pkg/docker"
 	"github.com/adrianliechti/loop/pkg/kubectl"
 	"github.com/adrianliechti/loop/pkg/kubernetes"
+	"github.com/adrianliechti/loop/pkg/ssh"
 	"github.com/adrianliechti/loop/pkg/to"
 	"github.com/google/uuid"
 
@@ -77,6 +83,67 @@ func runShell(ctx context.Context, client kubernetes.Client, path string, port i
 	}()
 
 	return kubectl.Attach(ctx, client.ConfigPath(), namespace, pod, "shell")
+}
+
+func startServer(ctx context.Context, path string, port int) (string, error) {
+	tool, _, err := docker.Tool(ctx)
+
+	if err != nil {
+		return "", err
+	}
+
+	args := []string{
+		"run",
+		"-d",
+
+		"--pull",
+		"always",
+
+		"--publish",
+		fmt.Sprintf("127.0.0.1:%d:22", port),
+
+		"--volume",
+		path + ":/src",
+
+		"adrianliechti/loop-tunnel",
+	}
+
+	cmd := exec.CommandContext(ctx, tool, args...)
+
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		return "", errors.New(string(output))
+	}
+
+	text := string(output)
+	text = strings.ReplaceAll(text, "\r", "")
+	text = strings.TrimRight(text, "\n")
+
+	lines := strings.Split(text, "\n")
+
+	if len(lines) == 0 {
+		return "", errors.New("unable to get container id")
+	}
+
+	container := lines[len(lines)-1]
+	return container, nil
+}
+
+func stopServer(ctx context.Context, container string) error {
+	tool, _, err := docker.Tool(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.CommandContext(ctx, tool, "rm", "--force", container)
+
+	if output, err := cmd.CombinedOutput(); err != nil {
+		return errors.New(string(output))
+	}
+
+	return err
 }
 
 func startPod(ctx context.Context, client kubernetes.Client, namespace, image string, stdin, tty bool) (string, error) {
@@ -162,4 +229,52 @@ func stopPod(ctx context.Context, client kubernetes.Client, namespace, name stri
 	})
 
 	return err
+}
+
+func runTunnel(ctx context.Context, client kubernetes.Client, namespace, name string, port int, tunnels map[int]int) error {
+	ssh, _, err := ssh.Tool(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	kubectl, _, err := kubectl.Tool(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	args := []string{
+		"-q",
+		"-t",
+		"-l",
+		"root",
+		"-o",
+		"UserKnownHostsFile=/dev/null",
+		"-o",
+		"StrictHostKeyChecking=no",
+		"-o",
+		fmt.Sprintf("ProxyCommand=%s --kubeconfig %s exec -i -n %s %s -c ssh -- nc 127.0.0.1 22", kubectl, client.ConfigPath(), namespace, name),
+		"localhost",
+	}
+
+	command := "mkdir /mnt/src && sshfs -o allow_other -p 2222 root@localhost:/src /mnt/src && exec /bin/ash"
+
+	if port != 0 {
+		args = append(args, "-R", fmt.Sprintf("2222:127.0.0.1:%d", port))
+	}
+
+	for source, target := range tunnels {
+		args = append(args, "-L", fmt.Sprintf("%d:127.0.0.1:%d", source, target))
+	}
+
+	if command != "" {
+		args = append(args, command)
+	}
+
+	cmd := exec.CommandContext(ctx, ssh, args...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	return cmd.Run()
 }
