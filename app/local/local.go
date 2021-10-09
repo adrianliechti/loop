e296a794fd55e548eba2f191213a88d5a093a51a
Adrian Liechti <adrian@monobox.ch>
2021-10-10 00:03:55 +0200

    Local Development Servers (#4)
    * add some local dev servers
    
    * add grafana
    
    * add server port config
    
    * add jupyter
    
    * add local vscode
    
    * add minio server

---
+++ app/local/local.go
@@ -0,0 +1,31 @@
+package local
+
+import (
+	"github.com/adrianliechti/loop/app"
+	"github.com/adrianliechti/loop/pkg/cli"
+)
+
+var Command = &cli.Command{
+	Name:  "local",
+	Usage: "local development servers",
+
+	Category: app.CategoryDevelopment,
+
+	HideHelpCommand: true,
+
+	Subcommands: []*cli.Command{
+		mariadbCommand,
+		mongoDBCommand,
+		postgresCommand,
+
+		grafanaCommand,
+		influxdbCommand,
+		redisCommand,
+
+		minioCommand,
+		vaultCommand,
+
+		codeCommand,
+		jupyterCommand,
+	},
+}
---
+++ app/local/local_mongodb.go
@@ -0,0 +1,72 @@
+package local
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/adrianliechti/loop/app"
+	"github.com/adrianliechti/loop/pkg/cli"
+	"github.com/adrianliechti/loop/pkg/docker"
+)
+
+var mongoDBCommand = &cli.Command{
+	Name:  "mongodb",
+	Usage: "local MongoDB server",
+
+	Flags: []cli.Flag{
+		app.PortFlag,
+	},
+
+	Action: func(c *cli.Context) error {
+		port := app.MustRandomPort(c, 27017)
+		return startMongoDB(c.Context, port)
+	},
+}
+
+func startMongoDB(ctx context.Context, port int) error {
+	image := "mongo:5-focal"
+
+	if err := docker.Pull(ctx, image); err != nil {
+		return err
+	}
+
+	target := 27017
+
+	if port == 0 {
+		port = target
+	}
+
+	database := "db"
+	username := "root"
+	password := "notsecure"
+
+	cli.Info()
+
+	cli.Table([]string{"Name", "Value"}, [][]string{
+		{"Host", fmt.Sprintf("localhost:%d", port)},
+		{"database", database},
+		{"Username", username},
+		{"Password", password},
+		{"URL", fmt.Sprintf("mongodb://%s:%s@localhost:%d/%s", username, password, port, database)},
+	})
+
+	cli.Info()
+
+	options := docker.RunOptions{
+		Env: map[string]string{
+			"MONGO_INITDB_DATABASE":      database,
+			"MONGO_INITDB_ROOT_USERNAME": username,
+			"MONGO_INITDB_ROOT_PASSWORD": password,
+		},
+
+		Ports: map[int]int{
+			port: target,
+		},
+
+		// Volumes: map[string]string{
+		// 	name: "/data/db",
+		// },
+	}
+
+	return docker.RunInteractive(ctx, image, options)
+}
