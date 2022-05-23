d10a5de99173548b93898d8c620a6f330dc48318
Adrian Liechti <adrian@monobox.ch>
2022-05-24 01:09:13 +0200

    run dns server

--- app/connect/connect_sshuttle.go
+++ app/connect/connect_sshuttle.go
@@ -14,6 +14,7 @@ import (
 	"github.com/google/uuid"
 
 	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -49,7 +50,7 @@ func runShuttle(ctx context.Context, client kubernetes.Client, namespace string)
 		"--method",
 		"auto",
 		"--dns",
-		//"--to-ns=127.0.0.1",
+		"--to-ns=127.0.0.1",
 		"-r",
 		"root@localhost",
 		"-e",
@@ -75,6 +76,41 @@ func shuttleLabels(name string) map[string]string {
 func createShuttle(ctx context.Context, client kubernetes.Client, namespace, name string) (*corev1.Pod, error) {
 	labels := shuttleLabels(name)
 
+	account := &corev1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   name,
+			Labels: labels,
+		},
+	}
+
+	if _, err := client.CoreV1().ServiceAccounts(namespace).Create(ctx, account, metav1.CreateOptions{}); err != nil {
+		return nil, err
+	}
+
+	binding := &rbacv1.ClusterRoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   name,
+			Labels: labels,
+		},
+
+		RoleRef: rbacv1.RoleRef{
+			Kind: "ClusterRole",
+			Name: "cluster-admin",
+		},
+
+		Subjects: []rbacv1.Subject{
+			{
+				Kind:      rbacv1.ServiceAccountKind,
+				Name:      account.Name,
+				Namespace: namespace,
+			},
+		},
+	}
+
+	if _, err := client.RbacV1().ClusterRoleBindings().Create(ctx, binding, metav1.CreateOptions{}); err != nil {
+		return nil, err
+	}
+
 	pod := &corev1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:   name,
@@ -82,15 +118,17 @@ func createShuttle(ctx context.Context, client kubernetes.Client, namespace, nam
 		},
 
 		Spec: corev1.PodSpec{
+			ServiceAccountName: account.Name,
+
 			Containers: []corev1.Container{
 				{
 					Name:  "sshuttle",
 					Image: "adrianliechti/loop-tunnel",
 				},
-				// {
-				// 	Name:  "dns",
-				// 	Image: "adrianliechti/loop-dns",
-				// },
+				{
+					Name:  "dns",
+					Image: "adrianliechti/loop-dns",
+				},
 			},
 		},
 	}
@@ -106,6 +144,14 @@ func createShuttle(ctx context.Context, client kubernetes.Client, namespace, nam
 }
 
 func deleteShuttle(ctx context.Context, client kubernetes.Client, namespace, name string) error {
+	if err := client.RbacV1().ClusterRoleBindings().Delete(ctx, name, metav1.DeleteOptions{}); err != nil {
+		//return err
+	}
+
+	if err := client.CoreV1().ServiceAccounts(namespace).Delete(ctx, name, metav1.DeleteOptions{}); err != nil {
+		//return err
+	}
+
 	if err := client.CoreV1().Pods(namespace).Delete(ctx, name, metav1.DeleteOptions{}); err != nil {
 		//return err
 	}
