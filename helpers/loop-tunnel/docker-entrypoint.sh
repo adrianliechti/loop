5f4d9dbcb5cab31535c251c8d7407a00b0f064e4
Adrian Liechti <adrian@monobox.ch>
2022-06-27 21:34:59 +0200

    create keys if needed

--- helpers/loop-tunnel/docker-entrypoint.sh
+++ helpers/loop-tunnel/docker-entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-/usr/bin/ssh-keygen -q -N "" -t rsa -f /run/sshd/ssh_host_rsa_key
-/usr/bin/ssh-keygen -q -N "" -t ecdsa -f /run/sshd/ssh_host_ecdsa_key
-/usr/bin/ssh-keygen -q -N "" -t ed25519 -f /run/sshd/ssh_host_ed25519_key
+if [ ! -f /run/sshd/ssh_host_rsa_key ]; then
+    /usr/bin/ssh-keygen -q -N "" -t rsa -f /run/sshd/ssh_host_rsa_key
+fi
+
+if [ ! -f /run/sshd/ssh_host_ecdsa_key ]; then
+    /usr/bin/ssh-keygen -q -N "" -t ecdsa -f /run/sshd/ssh_host_ecdsa_key
+fi
+
+if [ ! -f /run/sshd/ssh_host_ed25519_key ]; then
+    /usr/bin/ssh-keygen -q -N "" -t ed25519 -f /run/sshd/ssh_host_ed25519_key
+fi
 
 /usr/sbin/sshd -D -d -e
