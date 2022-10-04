9166f9d9271c40270dc8e5782ce9917d136d0eaa
Adrian Liechti <adrian@monobox.ch>
2022-10-04 16:47:52 +0200

    better remote code

--- app/remote/remote_code.go
+++ app/remote/remote_code.go
@@ -4,12 +4,19 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/adrianliechti/loop/app"
 	"github.com/adrianliechti/loop/pkg/cli"
 	"github.com/adrianliechti/loop/pkg/kubernetes"
 	"github.com/adrianliechti/loop/pkg/system"
+	"github.com/adrianliechti/loop/pkg/to"
+	"github.com/google/uuid"
+
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var codeCommand = &cli.Command{
@@ -22,6 +29,10 @@ var codeCommand = &cli.Command{
 		&cli.StringFlag{
 			Name:  app.PortFlag.Name,
 			Usage: "local server port",
+		},
+		&cli.StringFlag{
+			Name:  "stack",
+			Usage: "language stack",
 		},
 
 		// &cli.StringSliceFlag{
@@ -39,6 +50,26 @@ var codeCommand = &cli.Command{
 			return err
 		}
 
+		stacks := []string{
+			"dotnet",
+			"golang",
+			"java",
+		}
+
+		stack := c.String("stack")
+
+		if stack == "" {
+			i, _, err := cli.Select("select stack", stacks)
+
+			if err != nil {
+				return err
+			}
+
+			stack = stacks[i]
+		}
+
+		stack = strings.ToLower(stack)
+
 		port := app.MustPortOrRandom(c, 3000)
 		namespace := app.Namespace(c)
 
@@ -46,11 +77,11 @@ var codeCommand = &cli.Command{
 			namespace = client.Namespace()
 		}
 
-		return runCode(c.Context, client, port, namespace, path, nil)
+		return runCode(c.Context, client, stack, port, namespace, path, nil)
 	},
 }
 
-func runCode(ctx context.Context, client kubernetes.Client, port int, namespace, path string, ports map[int]int) error {
+func runCode(ctx context.Context, client kubernetes.Client, stack string, port int, namespace, path string, ports map[int]int) error {
 	if namespace == "" {
 		namespace = client.Namespace()
 	}
@@ -74,7 +105,7 @@ func runCode(ctx context.Context, client kubernetes.Client, port int, namespace,
 	}()
 
 	cli.Infof("Starting remote VSCode...")
-	pod, err := startPod(ctx, client, namespace, "adrianliechti/loop-code", false, false)
+	pod, err := startCodeContainer(ctx, client, namespace, "adrianliechti/loop-code:"+stack)
 
 	if err != nil {
 		return err
@@ -82,7 +113,7 @@ func runCode(ctx context.Context, client kubernetes.Client, port int, namespace,
 
 	defer func() {
 		cli.Infof("Stopping remote VSCode (%s/%s)...", namespace, pod)
-		stopPod(context.Background(), client, namespace, pod)
+		stopCodeContainer(context.Background(), client, namespace, pod)
 	}()
 
 	time.AfterFunc(5*time.Second, func() {
@@ -96,4 +127,232 @@ func runCode(ctx context.Context, client kubernetes.Client, port int, namespace,
 	cli.Info("Press ctrl-c to stop remote VSCode server")
 
 	return runTunnel(ctx, client, namespace, pod, containerPort, tunnelPorts)
+}
+
+func startCodeContainer(ctx context.Context, client kubernetes.Client, namespace, image string) (string, error) {
+	name := "loop-code-" + uuid.New().String()[0:7]
+
+	mountPropagationBidirectional := corev1.MountPropagationBidirectional
+	mountPropagationHostToContainer := corev1.MountPropagationHostToContainer
+
+	if _, err := client.CoreV1().ServiceAccounts(namespace).Create(ctx, &corev1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+	}, metav1.CreateOptions{}); err != nil {
+		return "", err
+	}
+
+	if _, err := client.RbacV1().ClusterRoleBindings().Create(ctx, &rbacv1.ClusterRoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "ClusterRole",
+			Name:     "cluster-admin",
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind:      "ServiceAccount",
+				Name:      name,
+				Namespace: namespace,
+			},
+		},
+	}, metav1.CreateOptions{}); err != nil {
+		return "", err
+	}
+
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+
+		Spec: corev1.PodSpec{
+			ServiceAccountName: name,
+
+			InitContainers: []corev1.Container{
+				{
+					Name: "init-workspace",
+
+					Image:           "busybox:stable",
+					ImagePullPolicy: corev1.PullAlways,
+
+					Command: []string{
+						"chown",
+						"1000:1000",
+						"/mnt",
+					},
+
+					VolumeMounts: []corev1.VolumeMount{
+						{
+							Name:             "mnt",
+							MountPath:        "/mnt",
+							MountPropagation: &mountPropagationHostToContainer,
+						},
+					},
+				},
+			},
+
+			Containers: []corev1.Container{
+				{
+					Name: "code",
+
+					Image:           image,
+					ImagePullPolicy: corev1.PullAlways,
+
+					SecurityContext: &corev1.SecurityContext{
+						RunAsUser: to.Int64Ptr(1000),
+					},
+
+					Env: []corev1.EnvVar{
+						{
+							Name:  "DOCKER_HOST",
+							Value: "unix:///var/run/dind/docker.sock",
+						},
+					},
+
+					Ports: []corev1.ContainerPort{
+						{
+							Name:          "http",
+							Protocol:      corev1.ProtocolTCP,
+							ContainerPort: int32(3000),
+						},
+					},
+
+					VolumeMounts: []corev1.VolumeMount{
+						{
+							Name:             "mnt",
+							MountPath:        "/mnt",
+							MountPropagation: &mountPropagationHostToContainer,
+						},
+						{
+							Name:      "dind",
+							MountPath: "/var/run/dind",
+						},
+					},
+				},
+				{
+					Name: "docker",
+
+					Image:           "docker:20-dind",
+					ImagePullPolicy: corev1.PullAlways,
+
+					SecurityContext: &corev1.SecurityContext{
+						Privileged: to.BoolPtr(true),
+					},
+
+					Env: []corev1.EnvVar{
+						{
+							Name:  "DOCKER_TLS_CERTDIR",
+							Value: "",
+						},
+					},
+
+					Args: []string{
+						"--group",
+						"1000",
+						"--host",
+						"unix:///var/run/dind/docker.sock",
+					},
+
+					VolumeMounts: []corev1.VolumeMount{
+						{
+							Name:             "mnt",
+							MountPath:        "/mnt",
+							MountPropagation: &mountPropagationHostToContainer,
+						},
+						{
+							Name:      "dind",
+							MountPath: "/var/run/dind",
+						},
+						{
+							Name:      "docker",
+							MountPath: "/var/lib/docker",
+						},
+						{
+							Name:      "modules",
+							MountPath: "/lib/modules",
+							ReadOnly:  true,
+						},
+					},
+				},
+				{
+					Name: "ssh",
+
+					Image:           "adrianliechti/loop-tunnel",
+					ImagePullPolicy: corev1.PullAlways,
+
+					SecurityContext: &corev1.SecurityContext{
+						Privileged: to.BoolPtr(true),
+					},
+
+					VolumeMounts: []corev1.VolumeMount{
+						{
+							Name:             "mnt",
+							MountPath:        "/mnt",
+							MountPropagation: &mountPropagationBidirectional,
+						},
+					},
+				},
+			},
+
+			Volumes: []corev1.Volume{
+				{
+					Name: "mnt",
+					VolumeSource: corev1.VolumeSource{
+						EmptyDir: &corev1.EmptyDirVolumeSource{},
+					},
+				},
+				{
+					Name: "dind",
+					VolumeSource: corev1.VolumeSource{
+						EmptyDir: &corev1.EmptyDirVolumeSource{},
+					},
+				},
+				{
+					Name: "docker",
+					VolumeSource: corev1.VolumeSource{
+						EmptyDir: &corev1.EmptyDirVolumeSource{},
+					},
+				},
+				{
+					Name: "modules",
+					VolumeSource: corev1.VolumeSource{
+						HostPath: &corev1.HostPathVolumeSource{
+							Path: "/lib/modules",
+						},
+					},
+				},
+			},
+
+			TerminationGracePeriodSeconds: to.Int64Ptr(10),
+		},
+	}
+
+	pod, err := client.CoreV1().Pods(namespace).Create(ctx, pod, metav1.CreateOptions{})
+
+	if err != nil {
+		return "", err
+	}
+
+	if _, err := client.WaitForPod(ctx, pod.Namespace, pod.Name); err != nil {
+		return "", err
+	}
+
+	return pod.Name, nil
+}
+
+func stopCodeContainer(ctx context.Context, client kubernetes.Client, namespace, name string) error {
+	if err := client.CoreV1().ServiceAccounts(namespace).Delete(ctx, name, metav1.DeleteOptions{}); err != nil {
+		// return err
+	}
+
+	if err := client.RbacV1().ClusterRoleBindings().Delete(ctx, name, metav1.DeleteOptions{}); err != nil {
+		// return err
+	}
+
+	return client.CoreV1().Pods(namespace).Delete(ctx, name, metav1.DeleteOptions{
+		GracePeriodSeconds: to.Int64Ptr(0),
+	})
 }
