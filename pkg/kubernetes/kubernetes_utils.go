f8f2a0d449519f372d4a180805945c628771a1f7
Adrian Liechti <adrian@monobox.ch>
2024-06-30 01:06:18 +0200

    speed up pod/service readyness

--- pkg/kubernetes/kubernetes_utils.go
+++ pkg/kubernetes/kubernetes_utils.go
@@ -3,11 +3,15 @@ package kubernetes
 import (
 	"context"
 	"errors"
-	"time"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/tools/cache"
+	watchtools "k8s.io/client-go/tools/watch"
 )
 
 func (c *client) ServicePods(ctx context.Context, namespace, name string) ([]corev1.Pod, error) {
@@ -62,76 +66,107 @@ func (c *client) ServiceAddress(ctx context.Context, namespace, name string) (st
 }
 
 func (c *client) WaitForPod(ctx context.Context, namespace, name string) (*corev1.Pod, error) {
-	timeout := time.After(300 * time.Second)
-	ticker := time.NewTicker(5 * time.Second)
+	selector := fields.OneTermEqualSelector("metadata.name", name).String()
 
-LOOP:
-	for {
-		select {
-		case <-ctx.Done():
-			return nil, errors.New("cancelled")
-		case <-timeout:
-			return nil, errors.New("timeout")
-		case <-ticker.C:
-			pod, err := c.CoreV1().Pods(namespace).Get(ctx, name, metav1.GetOptions{})
+	lw := &cache.ListWatch{
+		ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+			options.FieldSelector = selector
+			return c.CoreV1().Pods(namespace).List(ctx, options)
+		},
 
-			if err != nil {
-				continue
+		WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+			options.FieldSelector = selector
+			return c.CoreV1().Pods(namespace).Watch(ctx, options)
+		},
+	}
+
+	ev, err := watchtools.UntilWithSync(ctx, lw, &corev1.Pod{}, nil, podReady)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return ev.Object.(*corev1.Pod), nil
+}
+
+func podReady(event watch.Event) (bool, error) {
+	switch event.Type {
+	case watch.Deleted:
+		return false, errors.New("pod deleted")
+	}
+	switch t := event.Object.(type) {
+	case *corev1.Pod:
+		switch t.Status.Phase {
+		case corev1.PodFailed, corev1.PodSucceeded:
+			return false, errors.New("pod completed")
+
+		case corev1.PodRunning:
+			conditions := t.Status.Conditions
+
+			if conditions == nil {
+				return false, nil
 			}
 
-			if pod.Status.Phase == corev1.PodFailed {
-				return pod, errors.New("pod failed")
-			}
-
-			if pod.Status.Phase == corev1.PodSucceeded {
-				return pod, errors.New("pod succeeded")
-			}
-
-			if pod.Status.Phase != corev1.PodRunning {
-				continue
-			}
-
-			for _, status := range pod.Status.ContainerStatuses {
-				if !status.Ready {
-					continue LOOP
+			for i := range conditions {
+				if conditions[i].Type == corev1.PodReady &&
+					conditions[i].Status == corev1.ConditionTrue {
+					return true, nil
 				}
 			}
-
-			return pod, nil
 		}
 	}
+
+	return false, nil
 }
 
 func (c *client) WaitForService(ctx context.Context, namespace, name string) (*corev1.Service, error) {
-	timeout := time.After(300 * time.Second)
-	ticker := time.NewTicker(5 * time.Second)
+	selector := fields.OneTermEqualSelector("metadata.name", name).String()
 
-	for {
-		select {
-		case <-ctx.Done():
-			return nil, errors.New("cancelled")
-		case <-timeout:
-			return nil, errors.New("timeout")
-		case <-ticker.C:
-			service, err := c.CoreV1().Services(namespace).Get(ctx, name, metav1.GetOptions{})
+	lw := &cache.ListWatch{
+		ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+			options.FieldSelector = selector
+			return c.CoreV1().Services(namespace).List(ctx, options)
+		},
 
-			if err != nil {
-				continue
-			}
-
-			if service.Spec.Type == corev1.ServiceTypeLoadBalancer {
-				if len(service.Status.LoadBalancer.Ingress) == 0 {
-					continue
-				}
-
-				ingress := service.Status.LoadBalancer.Ingress[0]
-
-				if ingress.IP == "" {
-					continue
-				}
-			}
-
-			return service, nil
-		}
+		WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+			options.FieldSelector = selector
+			return c.CoreV1().Services(namespace).Watch(ctx, options)
+		},
 	}
+
+	ev, err := watchtools.UntilWithSync(ctx, lw, &corev1.Service{}, nil, serviceReady)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return ev.Object.(*corev1.Service), nil
+}
+
+func serviceReady(event watch.Event) (bool, error) {
+	switch event.Type {
+	case watch.Deleted:
+		return false, errors.New("service deleted")
+	}
+	switch t := event.Object.(type) {
+	case *corev1.Service:
+		conditions := t.Status.Conditions
+
+		if t.Spec.Type == corev1.ServiceTypeLoadBalancer {
+			if len(t.Status.LoadBalancer.Ingress) == 0 {
+				return false, nil
+			}
+
+			ingress := t.Status.LoadBalancer.Ingress[0]
+
+			if ingress.IP == "" {
+				return false, nil
+			}
+		}
+
+		_ = conditions
+		return true, nil
+	}
+
+	return false, nil
 }
